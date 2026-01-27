010a9f5bd21d053203412940aa4164f7994e395b
kimminseo0210 <kimminseo@gimminseoui-MacBookAir.local>
2026-01-27 10:12:19 +0900

    first commit

---
+++ main/src/Main.java
@@ -0,0 +1,49 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Q2438
+//        int n;
+//        n = sc.nextInt();
+//
+//        for (int i=0; i<n; i++) {
+//            for (int j=0; j<i+1; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+
+        // Q1330
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//
+//        if (a < b){
+//            System.out.println("<");
+//        } else if (a>b) {
+//            System.out.println(">");
+//        } else {
+//            System.out.println("==");
+//        }
+
+        //Q2739
+//        int n = sc.nextInt();
+//        for(int i=1;i<10;i++){
+//            System.out.println(n+" * "+i+" = "+(n*i));
+//        }
+
+        // Q10952
+//        while (true) {
+//            int a = sc.nextInt();
+//            int b = sc.nextInt();
+//            if (a == 0 && b == 0) {
+//                break;
+//            }
+//            System.out.println( a + b);
+//        }
+
+        // Q11654
+        int s = sc.next().charAt(0);
+        System.out.println(s);
+
+        sc.close();
+    }
+}
