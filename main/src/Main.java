01cc814ab0e14d4ce03928970ed25c7c976f9abf
kimminseo0210 <kimminseo@gimminseoui-MacBookAir.local>
2026-01-28 10:09:10 +0900

    자바_문자열.md 작성

--- main/src/Main.java
+++ main/src/Main.java
@@ -44,8 +44,27 @@ public class Main {
 //        int s = sc.next().charAt(0);
 //        System.out.println(s);
 
-        // Q
+        // Q2439
+//        int n = sc.nextInt();
+//        for (int i=0; i<n ;i++) {
+//            for (int j=n-1; j>i; j--) {
+//                System.out.print(" ");
+//            }
+//            for (int k=0; k<=i; k++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
 
+        // Q11720
+        int numCount = sc.nextInt();
+        String numChar = sc.next();
+        int numSum = 0;
+
+        for (int i = 0; i < numCount; i++) {
+            numSum += numChar.charAt(i);
+        }
+        System.out.println(numSum);
         sc.close();
     }
 }
