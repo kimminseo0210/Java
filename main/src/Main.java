1b126e3c0bf8ed6b5bf428d8fc29b7fae9a24f86
kimminseo0210 <kimminseo@gimminseoui-MacBookAir.local>
2026-02-02 15:56:04 +0900

    ~Q10818, 삼항연산자.md 작성

--- main/src/Main.java
+++ main/src/Main.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
         Scanner sc = new Scanner(System.in);
         // Q2438
 //        int n;
@@ -94,12 +93,32 @@ public class Main {
 //        System.out.println(h+" "+m);
 
         // Q10250
-        int test_case = sc.nextInt();
-        for (int i=0; i<test_case; i++) {
+//        int testCase = Integer.parseInt(br.readLine());
+//        for (int i=0; i<testCase; i++) {
+//            StringTokenizer st = new StringTokenizer(br.readLine());
+//            int h = Integer.parseInt(st.nextToken()), w=Integer.parseInt(st.nextToken()),n=Integer.parseInt(st.nextToken());
+//
+//            if (n%h==0) System.out.println(h*100 + n/h);
+//            else System.out.println(n%h*100 + (n/h+1));
+//        }
 
+        // Q10818
+        int n = Integer.parseInt(br.readLine());    // 숫자 개수
+        // N 개의 정수 입력
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        // 최대 최소 -1,000,000~1,000,000
+        // 또는 첫 번째 값으로 초기화
+        int max = -1000001;
+        int min = 1000001;
 
-            System.out.println((floor*100)+(roomNumber+1));
+        for (int i=0; i<n; i++) {
+            int now = Integer.parseInt(st.nextToken());
+            // [실습 포인트] 삼항 연산자 사용
+            max = (now > max) ? now : max;
+            min = (now < min) ? now : min;
         }
+        System.out.println(min + " " + max);
+
         sc.close();
     }
 }
