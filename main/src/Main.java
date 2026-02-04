de214971a8baaf4bea116a6a427a48f2db1f9241
kimminseo0210 <kmsk030210@gmail.com>
2026-02-04 20:52:57 +0900

    Q2577,2675

--- main/src/Main.java
+++ main/src/Main.java
@@ -125,21 +125,61 @@ public class Main {
 //        System.out.println(st.countTokens());
 
         // 방법 2
-        String str = sc.nextLine();
-        int cnt = 0;
+//        String str = sc.nextLine();
+//        int cnt = 0;
+//
+//        for (int i=0; i<str.length(); i++) {
+//            if (str.charAt(i) == ' ') {
+//                cnt++;
+//            }
+//        }
+//        if (str.charAt(0) != ' ' && str.charAt(str.length()-1) != ' ') {
+//            cnt++;
+//        }
+//        if (str.charAt(0) == ' ' && str.charAt(str.length()-1) == ' ') {
+//            cnt--;
+//        }
+//        System.out.println(cnt);
 
-        for (int i=0; i<str.length(); i++) {
-            if (str.charAt(i) == ' ') {
-                cnt++;
-            }
-        }
-        if (str.charAt(0) != ' ' && str.charAt(str.length()-1) != ' ') {
-            cnt++;
-        }
-        if (str.charAt(0) == ' ' && str.charAt(str.length()-1) == ' ') {
-            cnt--;
-        }
-        System.out.println(cnt);
+        // Q2577
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        int c = sc.nextInt();
+//        int[] arr = new int[10];
+//        String num = String.valueOf(a*b*c);
+//
+//        for (int i=0; i<10; i++) {
+//            int count = 0;
+//            for (int j=0; j<num.length(); j++) {
+//                if (num.charAt(j)-'0' == i) count++;
+//            }
+//            System.out.println(count);
+//        }
+        // 방법 2
+//        int a = Integer.parseInt(br.readLine());
+//        int b = Integer.parseInt(br.readLine());
+//        int c = Integer.parseInt(br.readLine());
+//        String num = String.valueOf(a*b*c);
+//        for (int i=0; i<10; i++) {
+//            int count = 0;
+//            for (int j=0; j<num.length(); j++) {
+//                if (num.charAt(j)-'0' == i) count++;
+//            }
+//            System.out.println(count);
+//        }
+
+        // Q2675
+//        int n = sc.nextInt();
+//        for (int i=0; i<n; i++) {
+//           int r = sc.nextInt();
+//           String p = sc.next();
+//
+//           for (int j=0; j<p.length(); j++) {
+//               for (int k=0; k<r; k++) {
+//                   System.out.print(p.charAt(j));
+//               }
+//           }
+//        }
         sc.close();
     }
 }
